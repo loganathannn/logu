7898d362f06d5b9f66e4d0d2d3e1a000c2c4cec2
loganathannn <loguhari09@gmail.com>
2017-07-14 14:10:13 +0530

    Create Sumco.java

---
+++ Sumco.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Sumco {
+public static void main(String[] args)
+{ 
+	int c=0;
+	Scanner sc=new Scanner(System.in);
+	int n=sc.nextInt();
+	for(int i=0;i<=n;i++)
+	{
+		
+		c=c+i;
+	}
+
+	System.out.print(c);
+}
+}
